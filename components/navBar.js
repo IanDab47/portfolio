1b1aea54c92aa21dfe7591ea10dfcd6860e0682f
iandab47 <dabinett.ian@gmail.com>
2022-10-23 23:54:39 -0500

    adds light mode styling and some mobile support

--- components/navBar.js
+++ components/navBar.js
@@ -4,9 +4,13 @@ import Link from 'next/link'
 export default function NavBar() {
   return (
     <nav style={{position: 'fixed', top: 0, zIndex: 10}}>
-      <Link href='#home'>Home</Link>
+      <Link href='#home'>Home - </Link>
 
-      <Link href='#about'>About Me</Link>
+      <Link href='#about'>About Me - </Link>
+
+      <Link href='#skills'>Skills - </Link>
+
+      <Link href='#projects'>Projects</Link>
     </nav>
   )
 }
