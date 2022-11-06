08fe0c6ac608460d2e92ce9cdc5fea41f99f93ed
IanDab47 <dabinett.ian@gmail.com>
2022-11-06 17:42:42 -0600

    Adds projects page for desktop

--- components/Projects/projects.js
+++ components/Projects/projects.js
@@ -2,18 +2,106 @@
 import main from '../../styles/Home.module.css'
 import styles from '../../styles/Projects.module.css'
 
+// React
+import { useState, useEffect } from 'react'
+
+// Components
+import Tab from './tab'
+
+import { projects } from '../../utils/projects'
+
 export default function Projects({ isDark, loading, loadedPage }) {
+  // State
+  const [tab, setTab] = useState('global keebs')
+  const [tmb, setTmb] = useState(projects[tab].images[0])
+  const [tabKeys, setTabKeys] = useState(Object.keys(projects))
+  const [colorSelect, setColorSelect] = useState(0)
+
+  // console.log(projects[tab].images.length)
+
+  // Hooks
+  useEffect(() => {
+    // use index of tab to select color of tab bg
+    setColorSelect(tabKeys.indexOf(tab) % 5)
+    // set main display to first image of project
+    setTmb(projects[tab].images[0])
+  }, [tab])
+
+  // Handlers
+  const selectImage = (e) => {
+    setTmb('/Thumbnails' + e.target.src.split('Thumbnails')[1])
+  }
+  
+  // Output
+  const color = colorSelect === 0 ? styles.colorOne :
+                colorSelect === 1 ? styles.colorTwo :
+                colorSelect === 2 ? styles.colorThree :
+                colorSelect === 3 ? styles.colorFour :
+                colorSelect === 4 ? styles.colorFive : null
+
+  const tabDisplay = tabKeys.map((filter, i) => {
+    return (
+      <Tab 
+        key={`tab_${i}`}
+        i={i}
+        filter={filter}
+        color={color}
+        tab={tab}
+        setTab={setTab}
+      />
+    )
+  })
+
   return (
     <section id='projects' 
       className={`
-      ${styles.projectsWrapper} 
-      ${main.revealPage}
-      ${main.waiting}
-      ${loading ? null : main.hidden}
-      ${loadedPage ? main.display : null}
-  `}
+        ${styles.projectsPage} 
+        ${isDark ? styles.dark : null}
+      `}
     >
-      Section about projects
+      <article 
+        className={`
+          ${styles.projectsWrapper}
+          ${main.revealPage}
+          ${main.waiting}
+          ${loading ? null : main.hidden}
+          ${loadedPage ? main.display : null}
+        `}
+      >
+        <nav className={styles.tabBar}>{tabDisplay}</nav>
+
+        <div className={`${styles.info}`}>
+          <header>
+            <p>{projects[tab].title}</p>
+
+            <p>{projects[tab].text}</p>
+
+            <a href={projects[tab].link} rel='noreferrer' target='_blank' 
+              className={styles.button}
+              style={{ '--color': projects[tab].color }}
+            >
+              Go To Site
+            </a>
+          </header>
+
+          <section>
+            <img src={tmb}/>
+
+            <div style={{ '--size': projects[tab].images.length }}
+            >
+              {projects[tab].images.map((image, i) => 
+                <img 
+                  key={`light_${i}`} 
+                  src={image} 
+                  title={`${projects[tab].title}_${i+1}`} 
+                  layout='fill' 
+                  onClick={selectImage}
+                />)}
+            </div>
+          </section>
+        </div>
+
+      </article>
     </section>
   )
 }
---
+++ utils/projects.js
@@ -0,0 +1,29 @@
+export const projects = {
+  'global keebs': {
+    title: `Global Keebs`,
+    text: `The all-in-one app to view the current keyboard listings on the web. This app takes the listings from r/mechmarket and compacts them into an orgainzed and clean browsing experience, separating listings by flair type and displaying all the details in a neat and intuitive way.`,
+    images: [`/Thumbnails/GlobalKeebs/home.PNG`, `/Thumbnails/GlobalKeebs/display.PNG`],
+    link: `https://global-keebs-iandab47.koyeb.app/`,
+    color: `#d62828`
+  },
+  bloom: {
+    title: `Bloom`,
+    text: `Everyone is an expert in their own field. We would like to expand on the gig economy to empower users to teach others the secrets of their trade. We love how social media has showcased, that there are many different ways to earn a living. There are experts in: AirBNB hosting, Cooking, Traveling, and even Coding. These experts can create a workshop for those who are curious in the field.`,
+    images: [`/Thumbnails/Bloom/home.PNG`, `/Thumbnails/Bloom/profile.PNG`],
+    link: `https://808-bloom.netlify.app/`,
+    color: `#898e59`
+  },
+  'pick the color': {
+    title: `:: pick the color ::`,
+    text: `"pick the color" is a simple idea with many different ways to play. There are 4 different modes for you to choose from: Multiplayer, Speedrun, Adventure, and Practice. Each mode will test you on how well you can determine the values of a digital color.`,
+    images: [`/Thumbnails/PickTheColor/home.PNG`, `/Thumbnails/PickTheColor/dom.PNG`, `/Thumbnails/PickTheColor/canvas.PNG`],
+    link: `https://iandab47.github.io/p1-project-folder/`,
+    color: `#33393f`
+  },
+  // wag: {
+  //   title: `W.A.G. (Working Algorithm Games)`,
+  //   text: ``,
+  //   images: [``],
+  //   link: ``
+  // }
+}
